38d088f676dec7f7c08f7c94fcd0d377207dd8d5
JimeVillca <jimena.villca2@gmail.com>
2020-07-02 20:23:06 -0300

    cambios

--- recibos.sql
+++ recibos.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-06-2020 a las 04:42:42
--- Versión del servidor: 10.1.37-MariaDB
--- Versión de PHP: 7.1.26
+-- Tiempo de generación: 03-07-2020 a las 00:18:25
+-- Versión del servidor: 10.4.6-MariaDB
+-- Versión de PHP: 7.1.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,17 +30,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categoria` (
   `idcategoria` int(2) NOT NULL,
-  `descripcion` varchar(150) NOT NULL,
-  `porcentaje` int(11) NOT NULL,
-  `tipo` tinyint(1) NOT NULL
+  `descripcion` varchar(150) DEFAULT NULL,
+  `porcentaje` varchar(11) DEFAULT NULL,
+  `gratificacion` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `categoria`
 --
 
-INSERT INTO `categoria` (`idcategoria`, `descripcion`, `porcentaje`, `tipo`) VALUES
-(1, 'Jubilacion', 10, 1);
+INSERT INTO `categoria` (`idcategoria`, `descripcion`, `porcentaje`, `gratificacion`) VALUES
+(1, 'Jubilacion', '10', 1),
+(4, 'Seguro', '20', NULL);
 
 -- --------------------------------------------------------
 
@@ -50,10 +51,18 @@ INSERT INTO `categoria` (`idcategoria`, `descripcion`, `porcentaje`, `tipo`) VAL
 
 CREATE TABLE `tipodocumento` (
   `idtipodocumento` int(11) NOT NULL,
-  `descripcion` varchar(100) NOT NULL,
-  `abreviacion` varchar(15) NOT NULL,
+  `descripcion` varchar(150) NOT NULL,
+  `abreviatura` varchar(25) NOT NULL,
   `fechaAlta` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tipodocumento`
+--
+
+INSERT INTO `tipodocumento` (`idtipodocumento`, `descripcion`, `abreviatura`, `fechaAlta`) VALUES
+(1, 'Documento Nacional de Identidad', 'DNI', '2020-06-15'),
+(2, 'Passaporte', 'PASS', '2020-06-15');
 
 -- --------------------------------------------------------
 
@@ -66,7 +75,7 @@ CREATE TABLE `usuario` (
   `nombre` varchar(150) NOT NULL,
   `apellido` varchar(150) NOT NULL,
   `usuario` varchar(50) NOT NULL,
-  `tipodocumento` int(2) NOT NULL,
+  `idtipodocumento` int(2) NOT NULL,
   `nrodocumento` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `fechaAlta` date NOT NULL
@@ -76,10 +85,12 @@ CREATE TABLE `usuario` (
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`idusuario`, `nombre`, `apellido`, `usuario`, `tipodocumento`, `nrodocumento`, `email`, `fechaAlta`) VALUES
+INSERT INTO `usuario` (`idusuario`, `nombre`, `apellido`, `usuario`, `idtipodocumento`, `nrodocumento`, `email`, `fechaAlta`) VALUES
 (1, 'Daniel', 'Granizado', 'dgranizado', 1, 20458745, 'dgranizado@gmail.com', '2020-06-01'),
 (2, 'Elizabeth', 'Grant', 'egrant', 1, 21452146, 'egrant@gmail.com', '2020-06-10'),
-(3, 'Juan', 'Perz', 'jperez', 1, 23145256, 'jperez@gmail.com', '2020-06-09');
+(3, 'Juan', 'Perz', 'jperez', 1, 23145256, 'jperez@gmail.com', '2020-06-09'),
+(4, 'Carolina', 'Aradra', 'caradra', 1, 30124582, 'cadaito@gmail.com', '2020-06-22'),
+(5, 'Carlos', 'Subiño', 'csubiño', 1, 34512120, 'csubiño@gmail.com', '2020-06-22');
 
 --
 -- Índices para tablas volcadas
@@ -111,19 +122,19 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `idcategoria` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idcategoria` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `tipodocumento`
 --
 ALTER TABLE `tipodocumento`
-  MODIFY `idtipodocumento` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idtipodocumento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
